5595e62aa99916953df625030c6f5b4dd20e6666
dinesh-8680 <dinesh.arunachalam@zohocorp.com>
2021-10-11 23:42:45 +0530

    Optimized Solution for Most Common Word

--- src/dev/dinesh/leetcode/amazon/MostCommonWord.java
+++ src/dev/dinesh/leetcode/amazon/MostCommonWord.java
@@ -1,25 +1,36 @@
 package dev.dinesh.leetcode.amazon;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class MostCommonWord {
     public String mostCommonWord(String paragraph, String[] banned) {
-        HashSet<String> bannedWords = new HashSet<>();
+        HashMap<String, Integer> words = new HashMap<>();
         for(String word: banned) {
-            bannedWords.add(word);
+            words.put(word, -1);
         }
-        HashMap<String, Integer> counter = new HashMap<>();
-        for(String word: paragraph.replaceAll("[^a-zA-Z]", " ").toLowerCase().split(" ")) {
-            if(!bannedWords.contains(word)) {
-                counter.put(word, counter.getOrDefault(word, 0) + 1);
+        StringBuilder para = new StringBuilder();
+        int maxCnt = 0;
+        String result = null;
+        for(Character ch : paragraph.toCharArray()) {
+            if(ch >= 'a' && ch <= 'z') {
+                para.append(ch);
+            } else if(ch >= 'A' && ch <= 'Z') {
+                para.append((char)(ch + 32));
+            } else {
+                String word = para.toString();
+                int cnt = words.getOrDefault(word, 0);
+                if(cnt != -1 && !word.equals("") && maxCnt < ++cnt) {
+                    maxCnt = cnt;
+                    result = word;
+                }
+                words.put(word, cnt);
+                para = new StringBuilder();
             }
         }
-        String result = "";
-        for(String key: counter.keySet()) {
-            if(result.equals("") || counter.get(key) > counter.get(result)) {
-                result = key;
-            }
+        String word = para.toString();
+        int cnt = words.getOrDefault(word, 0);
+        if(word.length() > 0 && maxCnt <= cnt){
+            return word;
         }
         return result;
     }
