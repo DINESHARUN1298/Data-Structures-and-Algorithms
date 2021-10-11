068027fa763ea3e3609393478161987a1c7bdfac
dinesh-8680 <dinesh.arunachalam@zohocorp.com>
2021-10-12 00:09:33 +0530

    Optimized Solution for Unique Email Addresses

--- src/dev/dinesh/leetcode/amazon/UniqueEmailAddresses.java
+++ src/dev/dinesh/leetcode/amazon/UniqueEmailAddresses.java
@@ -9,16 +9,21 @@ public class UniqueEmailAddresses {
             StringBuilder address = new StringBuilder();
             for(int index = 0; index < email.length(); index++) {
                 char ch = email.charAt(index);
+                int length = email.length();
                 if(ch == '.') {
                     continue;
                 } else if(ch == '+') {
                     while(email.charAt(index) != '@') {
                         index++;
                     }
-                    address.append(email.substring(index));
+                    while(index < length){
+                        address.append(email.charAt(index++));
+                    }
                     break;
                 } else if(ch == '@')  {
-                    address.append(email.substring(index));
+                    while(index < length){
+                        address.append(email.charAt(index++));
+                    }
                     break;
                 } else  {
                     address.append(ch);
