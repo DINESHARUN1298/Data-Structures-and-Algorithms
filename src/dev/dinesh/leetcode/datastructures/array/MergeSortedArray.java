ae1fff797b1e23a4aed16192631ea7973cb5cfea
dinesh-8680 <dinesh.arunachalam@zohocorp.com>
2021-10-19 09:28:00 +0530

    Solution for Merge Sorted Array

---
+++ src/dev/dinesh/leetcode/datastructures/array/MergeSortedArray.java
@@ -0,0 +1,14 @@
+package dev.dinesh.leetcode.datastructures.array;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int firstArrayIndex = m - 1, secondArrayIndex = n - 1, actualIndex = m + n - 1;
+        while(secondArrayIndex >= 0) {
+            if(firstArrayIndex >= 0 && nums1[firstArrayIndex] > nums2[secondArrayIndex]) {
+                nums1[actualIndex--] = nums1[firstArrayIndex--];
+            } else {
+                nums1[actualIndex--] = nums2[secondArrayIndex--];
+            }
+        }
+    }
+}
