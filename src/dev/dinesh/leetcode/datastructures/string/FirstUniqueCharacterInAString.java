131e87c064003c4d60fe9f95408c17223788334e
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-27 01:57:48 +0530

    Solution for First Unique Character In A String

---
+++ src/dev/dinesh/leetcode/datastructures/string/FirstUniqueCharacterInAString.java
@@ -0,0 +1,30 @@
+package dev.dinesh.leetcode.datastructures.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> counter = new HashMap<>();
+        for(int index = 0; index < s.length(); index++) {
+            Character ch = s.charAt(index);
+            if(!counter.containsKey(ch)) {
+                counter.put(s.charAt(index), index);
+            } else {
+                counter.put(s.charAt(index), -1);
+            }
+        }
+
+        int minIndex = Integer.MAX_VALUE;
+
+        for(char ch: counter.keySet()) {
+            int currentValue = counter.get(ch);
+            if(currentValue > -1 && currentValue < minIndex) {
+                minIndex = currentValue;
+            }
+        }
+
+        return minIndex == Integer.MAX_VALUE ? -1 : minIndex;
+
+    }
+}
