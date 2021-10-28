ee1f629b7e7d49dd57a6e86da1d462f40402c430
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-29 00:40:31 +0530

    Solution for Binary Tree InOrder Traversal (Iterative Approach)

---
+++ src/dev/dinesh/leetcode/datastructures/tree/BinaryTreeInOrderTraversalIterativeApproach.java
@@ -0,0 +1,40 @@
+package dev.dinesh.leetcode.datastructures.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInOrderTraversalIterativeApproach {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while(current != null || !stack.isEmpty()) {
+            while(current != null) {
+                stack.add(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+
+}
