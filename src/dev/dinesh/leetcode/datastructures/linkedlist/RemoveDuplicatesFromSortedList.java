df6b2ac7a5fc10dc4fa3315f384a9a6aae813b3e
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-28 21:27:10 +0530

    Solution for Remove Duplicates From Sorted List

---
+++ src/dev/dinesh/leetcode/datastructures/linkedlist/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+package dev.dinesh.leetcode.datastructures.linkedlist;
+
+public class RemoveDuplicatesFromSortedList {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+}
