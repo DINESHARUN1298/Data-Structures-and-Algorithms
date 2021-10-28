2187831721f992503c6dd3f58bbacf45c5ecc746
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-29 00:14:57 +0530

    Solution for Remove LinkedList Elements

---
+++ src/dev/dinesh/leetcode/datastructures/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,30 @@
+package dev.dinesh.leetcode.datastructures.linkedlist;
+
+public class RemoveLinkedListElements {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode prev = sentinel, curr = head;
+        while(curr != null) {
+            if(curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return sentinel.next;
+    }
+
+}
