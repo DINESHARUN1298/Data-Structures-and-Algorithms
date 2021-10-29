df4eafe160d493293a981f59930f63fc89f38137
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-30 01:41:59 +0530

    Solution for Path Sum

---
+++ src/dev/dinesh/leetcode/datastructures/tree/PathSum.java
@@ -0,0 +1,31 @@
+package dev.dinesh.leetcode.datastructures.tree;
+
+public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if(root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+}
