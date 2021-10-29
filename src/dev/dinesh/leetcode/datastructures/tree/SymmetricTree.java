e37c4e5f5b0bacd55499152ffc3bdb3fcd91c16e
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-30 01:11:41 +0530

    Solution for Symmetric Tree

---
+++ src/dev/dinesh/leetcode/datastructures/tree/SymmetricTree.java
@@ -0,0 +1,34 @@
+package dev.dinesh.leetcode.datastructures.tree;
+
+public class SymmetricTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) {
+            return true;
+        }
+        if(t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+}
