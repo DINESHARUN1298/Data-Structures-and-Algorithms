b7a9d7f7dff75632d25b26c71e524a858852747f
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-31 16:33:13 +0530

    Solution for NumberOfIslands

---
+++ src/dev/dinesh/leetcode/preparation/amazon/NumberOfIslands.java
@@ -0,0 +1,31 @@
+package dev.dinesh.leetcode.preparation.amazon;
+
+public class NumberOfIslands {
+    public void dfs(char[][] grid, int rowIndex, int colIndex) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(rowIndex < 0 || rowIndex >= rows || colIndex < 0 || colIndex >= cols || grid[rowIndex][colIndex] == '0') {
+            return;
+        }
+        grid[rowIndex][colIndex] = '0';
+        dfs(grid, rowIndex+1, colIndex);
+        dfs(grid, rowIndex-1, colIndex);
+        dfs(grid, rowIndex, colIndex+1);
+        dfs(grid, rowIndex, colIndex-1);
+        return;
+    }
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int numberOfIslands = 0;
+        for(int rowIndex = 0; rowIndex < rows; rowIndex++) {
+            for(int colIndex = 0; colIndex < cols; colIndex++) {
+                if(grid[rowIndex][colIndex] == '1') {
+                    numberOfIslands++;
+                    dfs(grid, rowIndex, colIndex);
+                }
+            }
+        }
+        return numberOfIslands;
+    }
+}
