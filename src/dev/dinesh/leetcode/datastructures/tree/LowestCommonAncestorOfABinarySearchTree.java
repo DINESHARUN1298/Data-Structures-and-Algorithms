19972a372528079d7abbc8b0fd6562c53396737d
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-10-31 19:46:11 +0530

    Solution for LowestCommonAncestorOfABinarySearchTree

---
+++ src/dev/dinesh/leetcode/datastructures/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,33 @@
+package dev.dinesh.leetcode.datastructures.tree;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+        if(root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
+
+}
