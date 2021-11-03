785062762138a91b31edb09bb9227febc8a91202
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-11-03 11:09:53 +0530

    Solution for RotateArray

---
+++ src/dev/dinesh/leetcode/algorithms/twopointers/RotateArray.java
@@ -0,0 +1,20 @@
+package dev.dinesh.leetcode.algorithms.twopointers;
+
+public class RotateArray {
+
+    public void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+}
