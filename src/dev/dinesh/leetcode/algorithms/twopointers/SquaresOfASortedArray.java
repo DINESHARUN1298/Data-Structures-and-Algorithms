7b1a5b197c8b1444676f60041a5b725ace8069be
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-11-03 10:52:18 +0530

    Solution for SquaresOfASortedArray

---
+++ src/dev/dinesh/leetcode/algorithms/twopointers/SquaresOfASortedArray.java
@@ -0,0 +1,18 @@
+package dev.dinesh.leetcode.algorithms.twopointers;
+
+public class SquaresOfASortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 0, right = nums.length - 1, pointer = nums.length - 1;
+        while(left <= right) {
+            if(Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[pointer--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[pointer--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
