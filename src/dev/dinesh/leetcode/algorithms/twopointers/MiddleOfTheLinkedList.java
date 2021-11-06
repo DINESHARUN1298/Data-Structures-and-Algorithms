d622f917624b1d01400b81cfb94c3f8a954bc6c6
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-11-06 17:48:53 +0530

    Solution for MiddleOfTheLinkedList

---
+++ src/dev/dinesh/leetcode/algorithms/twopointers/MiddleOfTheLinkedList.java
@@ -0,0 +1,27 @@
+package dev.dinesh.leetcode.algorithms.twopointers;
+
+public class MiddleOfTheLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
