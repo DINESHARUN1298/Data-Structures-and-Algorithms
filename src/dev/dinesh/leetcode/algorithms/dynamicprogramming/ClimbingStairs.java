81c46d9b86bd2b5a9f58d2b81133dca17ba82cd4
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-11-15 01:36:23 +0530

    Solution for ClimbingStairs

---
+++ src/dev/dinesh/leetcode/algorithms/dynamicprogramming/ClimbingStairs.java
@@ -0,0 +1,13 @@
+package dev.dinesh.leetcode.algorithms.dynamicprogramming;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int index = 2; index <= n; index++) {
+            dp[index] = dp[index-1] + dp[index-2];
+        }
+        return dp[n];
+    }
+}
