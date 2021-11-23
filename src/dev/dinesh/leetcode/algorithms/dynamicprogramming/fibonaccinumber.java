acef5f7900dda5de9a47fc90a4269fe3181673e9
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-11-23 15:31:58 +0530

    Solution for Fibonacci Number

---
+++ src/dev/dinesh/leetcode/algorithms/dynamicprogramming/fibonaccinumber.java
@@ -0,0 +1,16 @@
+package dev.dinesh.leetcode.algorithms.dynamicprogramming;
+
+public class fibonaccinumber {
+    public int fib(int n) {
+        int[] fib = new int[n+1];
+        if(n <= 1) {
+            return n;
+        }
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int index = 2; index <= n; index++) {
+            fib[index] = fib[index-1] + fib[index-2];
+        }
+        return fib[n];
+    }
+}
