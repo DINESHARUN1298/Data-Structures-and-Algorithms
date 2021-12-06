860875ed83203cd1e030716aabbcaee8a85d6692
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-12-07 02:56:53 +0530

    Solution for MaximumDepthOfABinaryTree

---
+++ src/dev/dinesh/leetcode/companies/amazon/trees/MaximumDepthOfABinaryTree.java
@@ -0,0 +1,27 @@
+package dev.dinesh.leetcode.companies.amazon.trees;
+
+public class MaximumDepthOfABinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left_max = maxDepth(root.left) + 1;
+        int right_max = maxDepth(root.right) + 1;
+        return Math.max(left_max, right_max);
+    }
+
+}
