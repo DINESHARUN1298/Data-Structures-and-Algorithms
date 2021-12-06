38527d2e0ccc50aa98f86809f6e64428fee047a8
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-12-07 01:19:33 +0530

    Solution for BinaryTreePreorderTraversal

--- out/production/Data-Structures/ReadMe.md
+++ out/production/Data-Structures/ReadMe.md
@@ -2,4 +2,4 @@
 Data Structures are critical for implementing efficient solutions using programming languages. Here, I have shared my version of the DS implementations.These are inspired from William Fisset. 
 
 # Algorithms
-Algorithms are instances of logic to approach a problem and work towards an effective solution. 
+Algorithms are instances of logic to approach a problem and work towards an effective solution. Here, I have shared the algorithms and techniques of approach learnt from leetcode problems.
---
+++ src/dev/dinesh/leetcode/companies/amazon/trees/BinaryTreePreorderTraversal.java
@@ -0,0 +1,52 @@
+package dev.dinesh.leetcode.companies.amazon.trees;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePreorderTraversal {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if(root == null) {
+            return output;
+        }
+
+        stack.add(root);
+
+        while(!stack.isEmpty()) {
+
+            TreeNode currentNode = stack.pop();
+            output.add(currentNode.val);
+
+            if(currentNode.right != null) {
+                stack.add(currentNode.right);
+            }
+
+            if(currentNode.left != null) {
+                stack.add(currentNode.left);
+            }
+
+        }
+
+        return output;
+
+    }
+
+}
