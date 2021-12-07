05c6d1844f212d165d0d05cafe998f985dd25f06
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-12-07 13:33:44 +0530

    Solution for PathSum

---
+++ src/dev/dinesh/leetcode/companies/amazon/tree/PathSum.java
@@ -0,0 +1,34 @@
+package dev.dinesh.leetcode.companies.amazon.tree;
+
+public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root == null) {
+            return false;
+        }
+
+        targetSum -= root.val;
+
+        if(root.left == null && root.right == null && targetSum == 0) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+
+    }
+
+}
