d71eb850f720584fa125504501da12218ce88113
DINESHARUN1298 <dineshsurvives@gmail.com>
2021-12-13 22:44:06 +0530

    Solution for MeetingRoomsII

--- src/dev/dinesh/leetcode/companies/amazon/MeetingRoomsII.java
+++ src/dev/dinesh/leetcode/companies/amazon/MeetingRoomsII.java
@@ -10,22 +10,27 @@ public class MeetingRoomsII {
         if(intervals == null || intervals.length == 0) {
             return 0;
         }
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(intervals.length, new Comparator<Integer>(){
-            public int compare(Integer a, Integer b) {
-                return a - b;
-            }
-        });
-        Arrays.sort(intervals, new Comparator<int[]>() {
+        Arrays.sort(intervals, new Comparator<int[]>(){
             public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
-        priorityQueue.add(intervals[0][1]);
-        for(int index = 1; index < intervals.length; index++) {
-            if(intervals[index][0] >= priorityQueue.peek()) {
-                priorityQueue.poll();
+        PriorityQueue priorityQueue = new PriorityQueue<int[]>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[1] - b[1];
             }
-            priorityQueue.add(intervals[index][1]);
+        });
+        priorityQueue.add(intervals[0]);
+        for(int index = 1; index < intervals.length; index++) {
+            int[] prev = (int[]) priorityQueue.poll();
+            int[] curr = intervals[index];
+            if(prev[1] > curr[0]) {
+                priorityQueue.add(prev);
+                priorityQueue.add(curr);
+            } else {
+                prev[1] = curr[1];
+                priorityQueue.add(prev);
+            }
         }
         return priorityQueue.size();
     }
