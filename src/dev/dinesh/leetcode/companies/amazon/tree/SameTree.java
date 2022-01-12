a2c90adcebbec286263e7eda0c8fd827bdb24386
DINESHARUN1298 <dineshsurvives@gmail.com>
2022-01-12 13:40:36 +0530

    Solution for SameTree

---
+++ src/dev/dinesh/leetcode/companies/amazon/tree/SameTree.java
@@ -0,0 +1,41 @@
+package dev.dinesh.leetcode.companies.amazon.tree;
+
+public class SameTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+}
