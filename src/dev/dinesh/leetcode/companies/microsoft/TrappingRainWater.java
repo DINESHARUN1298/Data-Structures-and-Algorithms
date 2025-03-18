50d47efb22eb4dd9b8e7b3452d30609b8b0bfe6f
dinesh arun <dineshsurvives@gmail.com>
2025-03-19 01:55:39 +0530

    Solution for TrappingRainWater

---
+++ src/dev/dinesh/leetcode/companies/microsoft/TrappingRainWater.java
@@ -0,0 +1,29 @@
+package dev.dinesh.leetcode.companies.microsoft;
+
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while(left < right) {
+            if(height[left] < height[right]) {
+                left_max = Math.max(left_max, height[left]);
+                ans += (left_max - height[left]);
+                left++;
+            } else {
+                right_max = Math.max(right_max, height[right]);
+                ans += (right_max - height[right]);
+                right--;
+            }
+        }
+        return ans;
+    }
+
+}
+
+/**
+ * Solution: Two pointer algorithm
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
